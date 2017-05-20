4dbbef123bf4cb610ea0f60d31621f5026b8a978
Michal Karas <zoliqe@gmail.com>
2017-05-20 22:24:38 +0200

    first commit

---
+++ Arduino/sketch/sketch.ino
@@ -0,0 +1,115 @@
+#include <WebUSB.h>
+
+const WebUSBURL URLS[] = {
+  { 1, "zoliqe.github.io/remotesw/" },
+  { 0, "127.0.0.1:8081" },
+};
+
+const uint8_t ALLOWED_ORIGINS[] = { 1, 2 };
+
+WebUSB WebUSBSerial(URLS, 2, 1, ALLOWED_ORIGINS, 2);
+
+#define Serial WebUSBSerial
+
+const int ledPin = 13;
+
+bool state = false;
+unsigned long timeOut;
+
+void setup() {
+  while (!Serial) {
+    ;
+  }
+  Serial.begin(9600);
+  Serial.println("READY");
+  Serial.flush();
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, state);
+
+  Serial1.begin(9600);
+}
+
+void loop() {
+  if (Serial && Serial.available()) {
+    int val = Serial.read();
+    // Serial.write(val);
+    if (val == 'H') {
+      on();
+    } else if (val == 'L') {
+      off();
+    } else if (val == 'N') {
+      swstate();
+    }
+//    Serial.write("\r\n> ");
+  }
+  if (state) {
+    if (millis() > timeOut) {
+      off();
+    } else {
+      info();
+    }
+  }
+  delay(1000);
+}
+
+void on() {
+  state = HIGH;
+  timeOut = millis() + (10 * 60000); // 10 minutes
+  swstate();
+}
+
+void off() {
+  if (!state) {
+    return;
+  }
+  state = LOW;
+  swstate();
+}
+
+void swstate() {
+  Serial.println(state ? "HIGH" : "LOW");
+  digitalWrite(ledPin, state);
+  Serial.flush();
+}
+
+void info() {
+  unsigned long remains = timeOut - millis();
+  unsigned int mins = remains / 60000;
+  unsigned int secs = (remains - mins * 60000) / 1000;
+  Serial.print(mins < 10 ? "T0" : "T");
+  Serial.print(mins);
+  Serial.print(secs < 10 ? ":0" : ":");
+  Serial.println(secs);
+  Serial.flush();
+
+  if (Serial1 && Serial1.available()) {
+    iqrg();
+  }
+}
+
+void iqrg() {
+    uint8_t buff[11];
+    uint8_t len = 0;
+    Serial.print("CIV=");
+    while (Serial1.available() && len < 11) {
+      buff[len] = Serial1.read();
+      Serial.print(buff[len]);
+      Serial.print(',');
+      len++;
+    }
+    Serial.println("");
+    if (buff[4] == 0 || buff[4] == 3) {
+      uint8_t MHZ_100 = (buff[8]);
+      MHZ_100 = MHZ_100 - (((MHZ_100 / 16) * 6));
+      uint8_t MHZ = (buff[7]);
+      MHZ = MHZ - (((MHZ / 16) * 6));
+      uint8_t KHZ = buff[6];
+      KHZ = KHZ - (((KHZ / 16) * 6));
+      uint8_t HZ = buff[5];
+      HZ = HZ - (((HZ / 16) * 6));
+      unsigned long QRG = ((MHZ * 10000) + (KHZ * 100) + (HZ * 1));
+      Serial.print("QRG=");
+      Serial.println(QRG);
+    }
+}
+
---
+++ src/remotesw.js
@@ -0,0 +1,92 @@
+class RemoteSwitch {
+  constructor(onStateChangeCallback, onTimeChangeCallback) {
+    this._state = false;
+    this._time = "00:00";
+    this.onStateChangeCallback = onStateChangeCallback;
+    this.onTimeChangeCallback = onTimeChangeCallback;
+    this._d("constructor()", "done");
+  }
+
+  connectRequest() {
+    webusb.requestPort().then(selectedPort => {
+      this._connect(selectedPort);
+    }).catch(error => {
+      console.error('Connection error (1): ' + error);
+      this._state = !this._state; // revert state back
+      this.onStateChangeCallback(this.state);
+    });
+  }
+
+  _connect(port) {
+    this._port = port;
+    this._d('device.productName', this._port.device_.productName);
+    this._port.connect().then(() => {
+      this._d('port', this._port);
+      this._port.onReceive = data => {
+        this._proceed(data);
+      };
+      this._port.onReceiveError = error => {
+        console.log('Receive error: ' + error);
+      };
+      this._sendState(); // set current state
+    }, error => {
+      console.log('Connection error (2): ' + error);
+      this._state = !this._state; // revert state back
+      this.onStateChangeCallback(this.state);
+    });
+  }
+
+  get state() {
+    return this._state;
+  }
+  set state(state) {
+    this._d("state", state);
+    if (this._state == state) { // state already set, don't send anything
+      return;
+    }
+    
+    this._state = state;
+    if (this._port == undefined) {
+      this.connectRequest();
+    } else {
+      this._sendState();
+    }
+  }
+
+  get time() {
+    if (this._port == undefined || !this._state) {
+      return "00:00";
+    }
+    return this._time;
+  }
+  // set time(time) {
+  //   this._time = time;
+  //   this._d("time", this.time);
+  // }
+  
+  _proceed(value) {
+    this._d('data', data);
+    if (value.startsWith("LOW")) {
+      this._state = false;
+      this._d("state", state);
+      this.onStateChangeCallback(this.state);
+    } else if (value.startsWith("HIGH")) {
+      this._state = true;
+      this._d("state", state);
+      this.onStateChangeCallback(this.state);
+    } else if (value.startsWith("T")) {
+      this._time = value.substring(1);
+      this._d("time", this._time);
+      this.onTimeChangeCallback(this.time);
+    }
+  }
+
+  _sendState() {
+    this._port.send(this._state ? "H" : "L");
+  }
+
+  _d(what, value) {
+    console.log(what + "=" + value);
+  }
+}
+
